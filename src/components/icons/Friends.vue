7cb648ee71f23123a1eddfc5344ebc23359af366
ChinHongTan <chinhongtan1@gmail.com>
2022-05-22 23:30:03 +0800

    Create Friends.vue

---
+++ src/components/icons/Friends.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Friends</h1>
+<p>This are some of my best friends who gave me a lot of support. Without them, I won’t
+be able to learn and enjoy coding so much. I truely appreaciate them.</p>
+<p>NaozumiOuO #8298
+My very good friend  from Hong Kong who shared a lot of exciting knowledge with me.</p>
+<p>tommy2131 #3750
+Accompany me during my learning journey in coding
+Always excited about math and coding problems</p>
+<p>MiniApple #3344
+Teaches me a lot about various languages and their differences
+Has a wide knowledge of various languages</p>
+</template>
