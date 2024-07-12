6ac0731743f7ce7ba6730580b072ac409b25d753
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2024-07-12 17:48:54 +0800

    chore: Update eslint rules and configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,20 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
-  ],
-  env: {
-    "vue/setup-compiler-macros": true,
-  },
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript/recommended",
+        "@vue/eslint-config-prettier",
+        "plugin:prettier/recommended",
+    ],
+    env: {
+        "vue/setup-compiler-macros": true,
+    },
+    plugins: ["prettier"],
+    rules: {
+        indent: ["error", 4],
+        "prettier/prettier": ["error", { tabWidth: 4, useTabs: false }],
+    },
 };
